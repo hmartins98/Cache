562f4f42b8cc251e3764c8273b49089f81487caf
hmartins98 <hugo.alex.val.martins@hotmail.com>
2020-05-23 20:48:30 +0100

    Update RedisCacheInit.go

--- redis/RedisCacheInit.go
+++ redis/RedisCacheInit.go
@@ -4,16 +4,37 @@ import (
 	"github.com/go-redis/redis"
 )
 
+// //Cache struct
+// type Cache struct {
+// 	Cluster *redis.ClusterClient
+// 	Addrs   []string
+// }
+
+// //Initialize init
+// func (c *Cache) Initialize() {
+// 	c.Cluster = redis.NewClusterClient(&redis.ClusterOptions{
+// 		Addrs: c.Addrs,
+// 	})
+// 	if err := c.Cluster.Ping().Err(); err != nil {
+// 		panic("Unable to connect to redis " + err.Error())
+// 	}
+// }
+
+// //Close TODO
+// func (c *Cache) Close() {
+// 	c.Cluster.Close()
+// }
+
 //Cache struct
 type Cache struct {
-	Cluster *redis.ClusterClient
-	Addrs   []string
+	Cluster *redis.Client
+	Addrs   string
 }
 
 //Initialize init
 func (c *Cache) Initialize() {
-	c.Cluster = redis.NewClusterClient(&redis.ClusterOptions{
-		Addrs: c.Addrs,
+	c.Cluster = redis.NewClient(&redis.Options{
+		Addr: c.Addrs,
 	})
 	if err := c.Cluster.Ping().Err(); err != nil {
 		panic("Unable to connect to redis " + err.Error())
