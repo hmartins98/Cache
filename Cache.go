9666aaafbdbac073ba98e1f8a604d570e81f9442
hmartins98 <hugo.alex.val.martins@hotmail.com>
2020-05-23 01:38:38 +0100

    Update Cache.go

--- Cache.go
+++ Cache.go
@@ -6,7 +6,7 @@ import "time"
 type ICache interface {
 	Initialize()
 	Close()
-	Get(key string, obj *interface{}) error
-	Set(key string, obj *interface{}, expiration time.Duration) error
+	Get(key string, obj interface{}) error
+	Set(key string, obj interface{}, expiration time.Duration) error
 	Delete(key string)
 }
